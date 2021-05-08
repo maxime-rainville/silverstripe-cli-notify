f9e41ec3511916e109f563c549e48b6b1b7fa03e
Maxime Rainville <maxime@silverstripe.com>
2021-05-09 00:08:16 +1200

    Add initial code base

---
+++ src/NotifyMiddleware.php
@@ -0,0 +1,63 @@
+<?php
+namespace MaximeRainville\SilverstripeCliNotify;
+
+use Joli\JoliNotif\Notification;
+use Joli\JoliNotif\NotifierFactory;
+use SilverStripe\Control\Director;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Control\HTTPResponse;
+use SilverStripe\Control\Middleware\HTTPMiddleware;
+
+class NotifyMiddleware implements HTTPMiddleware
+{
+    public function process(HTTPRequest $request, callable $delegate)
+    {
+        if (Director::is_cli() && preg_match('/^dev\//i', $request->getURL())) {
+            try {
+                $response = $delegate($request);
+                $this->notify($request, $response);
+                return $response;
+            } catch (Exception $ex) {
+
+                $this->notifyError($request->getURL());
+                throw $ex;
+            }
+        }
+
+        $delegate($request);
+
+    }
+
+    protected function notify(HTTPRequest $request, HTTPResponse $response): void
+    {
+        $notifier = NotifierFactory::create();
+        $notification = new Notification();
+
+        $url = $request->getURL();
+
+        $notification->setTitle('Silverstripe CMS');
+        if ($response->isError()) {
+            $notification
+                ->setBody("ERROR $url")
+                ->setIcon(__DIR__.'/error.png');
+        } else {
+            $notification
+                ->setBody("DONE $url")
+                ->setIcon(__DIR__.'/success.png');
+        }
+
+        $notifier->send($notification);
+    }
+
+    protected function notifyError(string $url)
+    {
+        $notifier = NotifierFactory::create();
+        $notification = new Notification();
+        $notification
+            ->setTitle('Silverstripe CMS')
+            ->setBody("ERROR $url")
+            ->setIcon(__DIR__.'/error.png');
+        $notifier->send($notification);
+    }
+
+}
